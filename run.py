48122eb14c8253b1359ca6222ac751b6576d0c18
Misheck Gogo <136022287+MisheckGalx@users.noreply.github.com>
2026-02-13 15:36:02 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
-from flask import Flask
-from app import create_app
-from app.models import db
+from app import create_app, db
 
-app = Flask(__name__, static_folder='app/static', static_url_path='/static')
+app = create_app()
 
 with app.app_context():
-	# create all database tables
-	db.create_all()
-
+    db.create_all()
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
